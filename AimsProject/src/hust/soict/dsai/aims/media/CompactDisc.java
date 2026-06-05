aba0f10d63a7e245c77a6c7d90cfb7a3094ac4cc
Quynh Duong <quynhduong13277@gmail.com>
2026-06-05 23:19:14 +0700

    Add files via upload

---
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -0,0 +1,56 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc implements Playable {
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<Track>();
+
+    public CompactDisc(int id, String title, String category, float cost, int length, String director, String artist) {
+        super(id, title, category, cost, length, director);
+        this.artist = artist;
+    }
+
+    public String getArtist() { return artist; }
+
+    public void addTrack(Track track) {
+        if (tracks.contains(track)) {
+            System.out.println("Track '" + track.getTitle() + "' already exists.");
+        } else {
+            tracks.add(track);
+            System.out.println("Track '" + track.getTitle() + "' added.");
+        }
+    }
+
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+            System.out.println("Track '" + track.getTitle() + "' removed.");
+        } else {
+            System.out.println("Track '" + track.getTitle() + "' not found.");
+        }
+    }
+
+    @Override
+    public int getLength() {
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing CD: " + this.getTitle() + " by " + this.getArtist());
+        System.out.println("CD length: " + this.getLength());
+        for (Track track : tracks) {
+            track.play();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CD - " + getTitle() + " - " + getCategory() + " - " + getDirector() + " - Artist: " + artist + " - Length: " + getLength() + ": " + getCost() + " $";
+    }
+}
